9e8511c9e0bdfba06b44e74531acaef6ef618c2c
FernandoJavier1214 <ferchojavier1214@gmail.com>
2025-01-14 19:54:49 +0000

    FernandoJavier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const { LibManifestPlugin } = require("webpack");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+let pronoun = ["the", "our", "ella"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".es", ".mx"];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < ext.length; m++) {
+        //if(noun.endsWith(endPosition(1)))
+         console.log(pronoun[i] + adj[j] + noun[k] + ext[m]);
+        ;
+      }
+    }
+  }
+}
